8ed19ecbe42c30624d63403e29e24e449aa4bdd8
Vasyl <priymak17ukrvaha@gmail.com>
2025-11-02 16:08:05 +0200

    task done

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -57,7 +57,7 @@ export const PostDetails: React.FC<Props> = ({
 
         {comments.length === 0 &&
           loading !== Load.Comments &&
-          error !== ErrorType.DeleteComment && (
+          error !== ErrorType.FetchComments && (
             <p className="title is-4" data-cy="NoCommentsMessage">
               No comments yet
             </p>
